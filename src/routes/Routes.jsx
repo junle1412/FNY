6df1d9a0a445f34cf350e61865eedfdc6ef10f57
hieu <hieuldgcd18458@fpt.edu.vn>
2022-02-24 18:41:12 +0700

    commit

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import { Route ,Switch } from 'react-router-dom'
+import Home from '../pages/Home'
+import Catalog from '../pages/Catalog'
+import Cart from '../pages/Cart'
+import Product from '../pages/Product'
+
 const Routes = () => {
   return (
-    <div>Routes</div>
+   <Switch>
+     <Route path='/' exact componet={Home}/>
+     <Route path='/catalog/:slug' exact componet={Product}/>
+     <Route path='/catalog' exact componet={Catalog}/>
+     <Route path='/cart' exact componet={Cart}/>
+   </Switch>
   )
 }
 
